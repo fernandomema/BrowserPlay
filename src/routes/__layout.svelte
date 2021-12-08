3a2e9e9f14058ae18f3cb425d5a18e6ba32f8bd2
fernandomema <fernandomema@gmail.com>
2021-12-08 13:28:41 +0100

    Fullscreen on play game

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,4 @@
   import '../app.css';
 </script>
 
-{#if (window.location.hostname != 'localhost')}
-  <GoogleAnalytics properties={[ 'G-DZ1WT5YXMP' ]} />
-{/if}
 <slot />
--- src/routes/api/game/[game].js
+++ src/routes/api/game/[game].js
@@ -1,12 +1,28 @@
 import clientPromise from '../../../lib/db.js';
 
 export async function get (request) {
+     if (!request.params.game) return {
+        status: 400,
+        body: {
+            error: 'Missing game parameter'
+        }
+    }
+    
     const dbConnection = await clientPromise;
     const db = dbConnection.db();
     const collection = db.collection('games');
     const game = await collection.findOne({name: request.params.game});
-    return {
-        status: 200,
-        body: game
+    if (game) {
+        return {
+            status: 200,
+            body: game
+        }
+    } else {
+        return {
+            status: 404,
+            body: {
+                error: 'Game not found'
+            }
+        }
     }
 }
